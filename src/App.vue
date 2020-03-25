e4ae2d8914af615bb48e5b7c099630e5ca592729
QuangDung0508 <quangdung17111997@gmail.com>
2020-03-25 17:11:56 +0700

    create project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sudoku />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sudoku from './components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Sudoku
   }
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,225 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="sudoku">
+    <div class="row">
+      <h2>Sudoku</h2>
+      <strong>Time: {{formattedTime}}</strong>
+      <select class="select" v-model="difficulty" @change="generatePuzzle()">
+        <option v-for="(display, level) in levels" :key="level" :value="level">{{display}}</option>
+      </select>
+    </div>
+    <div class="grid">
+      <div class="row" v-for="(row, rowIndex) in puzzle" :key="rowIndex">
+        <div class="cell" :class="{
+            'border-right': colIndex === 2 || colIndex === 5,
+            'border-bottom' : rowIndex === 2 || rowIndex === 5,
+            'original' : cell.original,
+            'active' : activeRow === rowIndex && activeCol === colIndex,
+            'invalid' : cell.value && isCellInvalid(rowIndex, colIndex, cell.value)
+          }" 
+            v-for="(cell, colIndex) in row" :key="colIndex"
+            @click="setCellActive(rowIndex, colIndex, cell.original)"
+          >
+        {{cell.value}}
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <button class="btn" v-for="value in Array(9).keys()" :key="value"
+        :disabled="activeRow === -1 || activeCol === -1"
+        @click="setCellValue(value + 1)"
+      >
+        {{value + 1}}
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+import { sudoku } from './sudoku'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'Sudoku',
+  data () {
+    return {
+      puzzle: [],
+      difficulty: 'easy',
+      activeRow: -1,
+      activeCol: -1,
+      levels: {
+        'easy':      'Easy',
+        'medium':    'Medium',
+        'hard':      'Hard',
+        'very-hard': 'Very Hard',
+        'insane':    'Insane',
+        'inhuman':   'Inhuman'
+      },
+      seconds: 0,
+      timer: null
+    }
+  },
+  mounted () {
+    this.generatePuzzle()
+  },
+  computed: {
+    formattedTime () {
+      let min = Math.floor(this.seconds / 60)
+      let sec = this.seconds % 60
+      if (min < 10) {
+        min = `0${min}`
+      }
+      if (sec < 10) {
+        sec = `0${sec}`
+      }
+      return `${min}: ${sec}`
+    }
+  },
+  methods: {
+    generatePuzzle () {
+      const boardString = sudoku.generate(this.difficulty)
+      this.puzzle = sudoku.board_string_to_grid(boardString)
+        .map(row => {
+          return row.map(cell => {
+            return {
+              value: cell !== '.' ? parseInt(cell) : null,
+              original: cell !== '.'
+            }
+          })
+        })
+        this.seconds = 0
+        clearInterval(this.timer)
+        this.timer = setInterval(() => {
+          this.seconds += 1
+        }, 1000)
+    },
+
+    setCellActive (row, col, original) {
+      if (original) {
+        return
+      }
+      if (this.activeRow === row && this.activeCol === col) {
+        this.activeRow = -1
+        this.activeCol = -1
+        return
+      }
+      this.activeRow = row
+      this.activeCol = col
+    },
+
+    setCellValue (value) {
+      this.puzzle[this.activeRow][this.activeCol].value = value
+      this.activeRow = -1
+      this.activeCol = -1
+      if (this.isGameComplete()) {
+        const msg = [
+          'Success!',
+          '',
+          `Difficulty: ${this.levels[this.difficulty]}`,
+          `Time: ${this.formattedTime}`
+        ]
+        alert(msg.join('\n'))
+        this.generatePuzzle()
+      }
+    },
+
+    isCellInvalid (row, col, value) {
+      if (!value) {
+        return true
+      }
+      for (let c = 0; c < 9; c +=1) {
+        if (this.puzzle[row][c].value === value && c !== col) {
+          return true
+        }
+      }
+      for (let r = 0; r < 9; r +=1) {
+        if (this.puzzle[r][col].value === value && r !== row) {
+          return true
+        }
+      }
+      const rowStart = Math.floor(row/3) * 3
+      const colStart = Math.floor(col/3) * 3
+      for(let r = rowStart; r < rowStart + 3; r += 1) {
+        for(let c= colStart; c< colStart + 3; c += 1) {
+          if(this.puzzle[r][c].value === value && !(r === row && c === col)) {
+            return true
+          }
+        }
+      }
+      return false
+    },
+
+    isGameComplete () {
+      for(let r = 0; r < 9; r += 1) {
+        for(let c = 0; c< 9; c += 1) {
+          if (this.isCellInvalid(r,c,this.puzzle[r][c].value)) {
+            return false
+          }
+        }
+      }
+      return true
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .sudoku {
+    width: 100%;
+    max-width: 420px;
+    margin: 0.5rem auto;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  .grid {
+    width: calc(9 * 40px);
+    margin: 0.5rem auto 1rem;
+  }
+  .row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .cell {
+    display: block;
+    width: 40px;
+    height: 40px;
+    box-sizing: border-box;
+    border: 1px solid #bbb;
+    font-size: 24px;
+    line-height: 40px;
+    text-align: center;
+    cursor: default;
+  }
+  .cell.border-right {
+    border-right-width: 3px;
+  }
+  .cell.border-bottom {
+    border-bottom-width: 3px;
+  }
+  .cell.original {
+    font-weight: bold;
+  }
+  .cell:not(.original) {
+    cursor: pointer;
+  }
+  .cell.active {
+    background-color: #00c !important;
+    color: #fff;
+  }
+  .cell.invalid {
+    background-color: #c00;
+    color: #fff;
+  }
+  .btn {
+    width: 38px;
+    height: 38px;
+    font-size: 24px;
+    cursor: pointer;
+    background-color: chocolate;
+    color: white;
+  }
+  .select {
+    padding: 6px;
+    border-radius: 6px;
+  }
+  .btn:disabled {
+    cursor: not-allowed;
+  }
 </style>
